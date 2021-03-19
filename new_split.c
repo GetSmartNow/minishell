b6aa0e56e9c70287f96570de0eb6ae1db1cee0cb
mvernius <f.maple@mail.ru>
2021-03-19 20:49:12 +0300

    debug count_words example in notes.txt

--- new_split.c
+++ new_split.c
@@ -20,15 +20,6 @@ static int		len_cur_word(char const *s, char sep)
 		}
 		if (*s && *s != sep)
 		{
-			//if (shield_count % 2 == 1)
-			//{
-			//	if (*s == '\'' || *s == '\"')
-			//	{
-			//		s++;
-			//		length++;
-			//		shield_count = 0;
-			//	}
-			//}
 			if (shield_count % 2 == 0)
 			{
 				if ((*s == '\'' || *s == '\"') && flag == 1)
@@ -46,24 +37,6 @@ static int		len_cur_word(char const *s, char sep)
 				shield_count = 0;
 			s++;
 			length++;
-			//if ((*s == '\'' || *s == '\"') && shield_count % 2 == 1)
-			//{
-			//	flag = 0;
-			//	shield_count = 0;
-			//}
-			//else if ((*s == '\'' || *s == '\"') && shield_count % 2 == 0)
-			//{
-			//	flag = 1;
-			//	shield_count = 0;
-			//}
-			//s++;
-			//length++;
-			//if (*s == sep && (flag == 1 || shield_count % 2 == 1))
-			//{
-			//	printf("SH COUNT: |%d|\n", shield_count);
-			//	s++;
-			//	length++;
-			//}
 		}
 		else if (*s == sep && shield_count % 2 == 1)
 		{
@@ -150,14 +123,18 @@ static size_t	create_str(const char *s, char c, char **arr, size_t *iter)
 			arr[*iter] = ft_substr(s, 0, len_word);
 			s += len_word;
 			*iter += 1;
-			//arr[*iter] = (char *)malloc(len_word + 1);
-			//if (NULL == arr[*iter])
-			//	return (1);
-			//i = 0;
-			//while (i < len_word && *s)
-			//	arr[*iter][i++] = *s++;
-			//arr[(*iter)++][i] = '\0';
-			//printf("FROM SPLIT: |%s|\n", arr[*iter - 1]);
+			if (NULL == arr[*iter])
+				return (1);
+			// len_word = len_cur_word(s, c);
+			// arr[*iter] = (char *)malloc(len_word + 1);
+			// if (NULL == arr[*iter])
+			// 	return (1);
+			// i = 0;
+			// while (i < len_word && *s)
+			// 	arr[*iter][i++] = *s++;
+			// arr[*iter][i] = '\0';
+			// *iter += 1;
+			// printf("FROM SPLIT: |%s|\n", arr[*iter - 1]);
 		}
 	}
 	return (0);
@@ -167,11 +144,13 @@ char			**ft_split_new(char const *s, char c)
 {
 	char	**arr;
 	size_t	iter;
+	int		num_words;
 
 	if (!s)
 		return (NULL);
 	iter = 0;
-	arr = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	num_words = count_words(s, c); //debug;
+	arr = (char **)malloc((num_words + 1) * sizeof(char *));
 	if (NULL == arr)
 		return (NULL);
 	if (create_str(s, c, arr, &iter))
@@ -179,17 +158,3 @@ char			**ft_split_new(char const *s, char c)
 	arr[iter] = NULL;
 	return (arr);
 }
-
-//int main(int argc, char *argv[])
-//{
-//	if (argc)
-//	{
-//		char **splited = ft_split_new(argv[1], '|');
-//		while (*splited)
-//		{
-//			printf("%s\n", *splited);
-//			splited++;
-//		}
-//		printf("%s\n", *splited);
-//	}
-//}
